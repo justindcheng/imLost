8a9e3af8f0df0923bd45d4169e131f2061748df4
Ho Ching Justin Cheng <hochingc@usc.edu>
2015-09-19 12:48:40 -0400

    Initial Commit

---
+++ imLost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imLost
+//
+//  Created by Ho Ching Justin Cheng on 9/19/15.
+//  Copyright © 2015 Ho Ching Justin Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ imLostTests/imLostTests.swift
@@ -0,0 +1,36 @@
+//
+//  imLostTests.swift
+//  imLostTests
+//
+//  Created by Ho Ching Justin Cheng on 9/19/15.
+//  Copyright © 2015 Ho Ching Justin Cheng. All rights reserved.
+//
+
+import XCTest
+@testable import imLost
+
+class imLostTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
