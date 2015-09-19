228220f11ba7d08547c6f6016696d196f884d61d
tempname1337 <ziranwu@usc.edu>
2015-09-19 12:44:27 -0700

    test post

---
+++ imLost/answerQuestions.swift
@@ -0,0 +1,22 @@
+//
+//  answerQuestions.swift
+//  imLost
+//
+//  Created by Ziran Wu on 9/19/15.
+//  Copyright © 2015 Ho Ching Justin Cheng. All rights reserved.
+//
+
+import Foundation
+
+class answerQuestions: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
