41e7ec46e549d60c77e4cd7c69ba1c6f2f4935ec
Ho Ching Justin Cheng <hochingc@usc.edu>
2015-09-19 13:00:30 -0400

    First commit: parse tested

--- imLost/AppDelegate.swift
+++ imLost/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("tlsWlYMQvHDiZM5cA8dfILYdiCpSKjWSyjijnH3X",
+            clientKey: "g8qpPxUJ61BMIQ1ZftLDljoEM73bzBFdOLVNFSrt")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
         return true
     }
 
